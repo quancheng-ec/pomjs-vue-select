b02d33e0b2d4192617a381fcc08cc62175162a89
焦哥 <jiaoyingjun@quancheng-ec.com>
2017-04-11 22:11:29 +0800

    init

---
+++ ButtonSelect.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="input-group"
+       :style="'position:relative;padding-bottom:0px;width:'+(width?width:250)+'px;'">
+  
+    <button type="button"
+            @click="openSelect"
+            class="btn  btn-info btn-outline">{{label}}</button>
+  
+    <data-item-list v-bind="$props"></data-item-list>
+  </div>
+</template>
+
+<script>
+import DataItemList from './DataList.vue';
+export default {
+  props: ['label', 'chosenList', 'url', 'isSingle', 'items', 'width', 'placeholder'],
+  data() {
+    return {
+    }
+  },
+  methods: {
+    openSelect(e) {
+      this.$children[0].openSelect(e);
+    }
+  },
+  components: {
+    DataItemList
+  }
+}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+</style>
---
+++ SearchSelect.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="input-group"
+       :style="'position:relative;padding-bottom:0px;width:'+(width?width:250)+'px;'">
+    <div @click="openSelect"
+         class="bootstrap-tagsinput"
+         style="width:100%"
+         id="search-select">
+      <template v-if="!selectItems.length">
+        请选择...
+      </template>
+      <template v-if="selectItems.length">
+        <span class="tag label label-default text-muted qc-label"
+              v-for="(item,index) in selectItems">{{item.label}}
+                                                        <a href="javascript:;"class="qc-tag-x" @click.stop="delSelect(index)">x</a>
+                                                  </span>
+      </template>
+      <data-item-list v-bind="$props"></data-item-list>
+    </div>
+  </div>
+</template>
+<script>
+import DataItemList from './DataList.vue';
+export default {
+  props: ['label', 'chosenList', 'url', 'isSingle', 'items', 'width', 'placeholder'],
+  data() {
+    return {
+    }
+  },
+  methods: {
+    openSelect(e) {
+      this.$children[0].openSelect(e);
+    }
+  },
+  components: {
+    DataItemList
+  }
+}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+</style>
