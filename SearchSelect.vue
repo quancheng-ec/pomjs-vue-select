38fef799c66a45ca48bb8178e4240533859fca78
焦哥 <jiaoyingjun@quancheng-ec.com>
2017-04-25 11:40:38 +0800

    fix

--- SearchSelect.vue
+++ SearchSelect.vue
@@ -10,8 +10,8 @@
       <template v-if="chosenList.length">
         <span class="tag label label-default text-muted qc-label"
               v-for="(item,index) in chosenList">{{ short(item.label) }}
-                                                                  <a href="javascript:;"class="qc-tag-x" @click.stop="delSelect(index)">x</a>
-                                                            </span>
+                                                                    <a href="javascript:;"class="qc-tag-x" @click.stop="delSelect(index)">x</a>
+                                                              </span>
       </template>
     </div>
     <data-item-list v-bind="$props"></data-item-list>
@@ -30,10 +30,10 @@ export default {
       if (this.width) {
         if (this.width === '100%') {
           return '100%';
-        }else{
+        } else {
           return this.width + 'px';
         }
-      }else{
+      } else {
         return '250px';
       }
     },
@@ -46,6 +46,8 @@ export default {
     short(label) {
       if (label.length > 6) {
         return label.substring(0, 3) + '...';
+      } else {
+        return label;
       }
     }
   },
